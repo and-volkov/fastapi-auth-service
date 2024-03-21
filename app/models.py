edc4aa195d06d69405bab2119dfb2911974c926b
Andrey Volkov <andrey.dreamwall@gmail.com>
2024-03-21 22:48:23 +0400

    add models

---
+++ app/models.py
@@ -0,0 +1,49 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import DeclarativeBase, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    is_superuser = Column(Boolean, default=False)
+
+    roles = relationship("UserRole", back_populates="user")
+
+
+class Service(Base):
+    __tablename__ = "services"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+    roles = relationship("UserRole", back_populates="service")
+
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+    users = relationship("UserRole", back_populates="role")
+
+
+class UserRole(Base):
+    __tablename__ = "user_roles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    service_id = Column(Integer, ForeignKey("services.id"))
+    role_id = Column(Integer, ForeignKey("roles.id"))
+
+    user = relationship("User", back_populates="roles")
+    service = relationship("Service", back_populates="roles")
+    role = relationship("Role", back_populates="users")
