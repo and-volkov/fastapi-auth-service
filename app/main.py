d2a3e28ec080a79480e09c5be8fc7acf832e10e7
Andrey Volkov <andrey.dreamwall@gmail.com>
2024-03-22 15:33:06 +0400

    add api prefix to all routes

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from contextlib import asynccontextmanager
 from fastapi import Depends, FastAPI
 
 from .db import check_db_connection, get_db
+from .routers import users as users_router
+from .settings import settings
 
 
 @asynccontextmanager
@@ -11,7 +13,16 @@ async def lifespan(app: FastAPI):
     yield
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    docs_url=f"{settings.API_V1_STR}/docs",
+    lifespan=lifespan,
+)
+
+app.include_router(
+    users_router.router, prefix=settings.API_V1_STR, tags=["users"]
+)
 
 
 @app.get("/", dependencies=[Depends(get_db)])
