8d9c7185ebcb58ee8b557e6a240b3b9b08cb8ff5
Andrey Volkov <andrey.dreamwall@gmail.com>
2024-03-22 15:33:54 +0400

    add crud operations for user

---
+++ app/crud.py
@@ -0,0 +1,56 @@
+from fastapi import HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+
+import app.models as models
+import app.schemas as schemas
+from app.settings import settings
+
+
+def is_email(email: str):
+    return True if "@" in email else False
+
+
+def get_user(db: Session, email_or_username: str):
+    if is_email(email_or_username):
+        user = (
+            db.query(models.User)
+            .filter(models.User.email == email_or_username)
+            .first()
+        )
+    else:
+        user = (
+            db.query(models.User)
+            .filter(models.User.username == email_or_username)
+            .first()
+        )
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    return user
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(
+        username=user.username,
+        email=user.email,
+        password=user.password,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def delete_user(db: Session, email_or_username: str):
+    user = get_user(db, email_or_username)
+    db.delete(user)
+    db.commit()
+    return user
