7d1bf801e84e706275eb504c7ed8dd5df36a199b
Andrey Volkov <andrey.dreamwall@gmail.com>
2024-03-22 15:34:20 +0400

    init

---
+++ app/routers/users.py
@@ -0,0 +1,53 @@
+from typing import Union
+
+from fastapi import APIRouter, Depends
+from fastapi.exceptions import HTTPException
+from sqlalchemy.orm import Session
+
+import app.crud as crud
+import app.schemas as schemas
+from app.db import get_db
+
+router = APIRouter(
+    responses={
+        404: {"description": "User not found"},
+        409: {"description": "User already exists"},
+    },
+)
+
+
+@router.post(
+    "/users/", response_model=schemas.UserCreateResponse, status_code=201
+)
+async def create_user(
+    db: Session = Depends(get_db),
+    user: schemas.UserCreate = Depends(schemas.UserCreate),
+):
+    try:
+        return crud.create_user(db, user)
+    except Exception as e:
+        raise HTTPException(status_code=409, detail="User already exists")
+
+
+@router.get("/users/{email_or_username}/", response_model=schemas.UserResponse)
+async def read_user(email_or_username: str, db: Session = Depends(get_db)):
+    user = crud.get_user(db, email_or_username)
+    user = schemas.UserResponse(**user.__dict__)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@router.get("/users/", response_model=schemas.UserList)
+async def read_users(db: Session = Depends(get_db)):
+    users = crud.get_users(db)
+    return {"users": users}
+
+
+@router.delete(
+    "/users/{email_or_username}/",
+    status_code=204,
+)
+async def delete_user(email_or_username: str, db: Session = Depends(get_db)):
+    user = crud.delete_user(db, email_or_username)
+    return user
