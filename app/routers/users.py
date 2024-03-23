2db850722c4215e153163363404cc84ba76719e8
Andrey Volkov <andrey.dreamwall@gmail.com>
2024-03-23 16:58:40 +0400

    refactor

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
 
@@ -6,18 +6,13 @@ import app.crud as crud
 import app.schemas as schemas
 from app.db import get_db
 
-from .auth import get_current_superuser
-
-router = APIRouter(
-    responses={
-        404: {"description": "User not found"},
-        409: {"description": "User already exists"},
-    },
-)
+router = APIRouter()
 
 
 @router.post(
-    "/users/", response_model=schemas.UserCreateResponse, status_code=201
+    "/users/",
+    response_model=schemas.UserCreateResponse,
+    status_code=status.HTTP_201_CREATED,
 )
 async def create_user(
     db: Session = Depends(get_db),
@@ -32,16 +27,28 @@ async def create_user(
 @router.get("/users/{email_or_username}/", response_model=schemas.UserResponse)
 async def read_user(email_or_username: str, db: Session = Depends(get_db)):
     user = crud.get_user(db, email_or_username)
-    user = schemas.UserResponse(**user.__dict__)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return user
+    user_roles = crud.get_user_service_role(db, user.id)  # type: ignore
+    user_resp = schemas.UserResponse(
+        id=user.id,
+        username=user.username,
+        email=user.email,
+        is_superuser=user.is_superuser,
+        service_roles=[
+            schemas.UserServiceRoleResponse(
+                service_id=role.service_id,  # type: ignore
+                role_id=role.role_id,  # type: ignore
+            )
+            for role in user_roles
+        ],
+    )
+    return user_resp
 
 
 @router.get("/users/", response_model=schemas.UserList)
 async def read_users(
     db: Session = Depends(get_db),
-    _: schemas.User = Depends(get_current_superuser),
 ):
     users = crud.get_users(db)
     return {"users": users}
@@ -54,3 +61,25 @@ async def read_users(
 async def delete_user(email_or_username: str, db: Session = Depends(get_db)):
     user = crud.delete_user(db, email_or_username)
     return user
+
+
+@router.post("/service_role/", status_code=status.HTTP_201_CREATED)
+async def add_service_role(
+    user_service_role: schemas.UserServiceRoleCreate = Depends(
+        schemas.UserServiceRoleCreate
+    ),
+    db: Session = Depends(get_db),
+):
+    crud.create_user_service_role(db, user_service_role)
+    return {"message": "Service role added"}
+
+
+@router.delete("/service_role/", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_service_role(
+    user_service_role: schemas.UserServiceRoleDelete = Depends(
+        schemas.UserServiceRoleDelete
+    ),
+    db: Session = Depends(get_db),
+):
+    crud.delete_user_service_role(db, user_service_role)
+    return {"message": "Service role deleted"}
