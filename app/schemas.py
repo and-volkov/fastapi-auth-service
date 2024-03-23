9370092e6dcc31a5c3f9720b95fa87eeb938f835
Andrey Volkov <andrey.dreamwall@gmail.com>
2024-03-23 16:58:13 +0400

    refactor

--- app/schemas.py
+++ app/schemas.py
@@ -3,6 +3,82 @@ from typing import List, Optional
 from pydantic import BaseModel, EmailStr
 
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class Service(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+
+class ServiceCreate(BaseModel):
+    name: str
+
+
+class ServiceList(BaseModel):
+    services: List[Service]
+
+    class Config:
+        from_attributes = True
+
+
+class Role(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+
+class RoleCreate(BaseModel):
+    name: str
+
+
+class RoleList(BaseModel):
+    roles: List[Role]
+
+    class Config:
+        from_attributes = True
+
+
+class UserServiceRole(BaseModel):
+    id: int
+    user_id: int
+    role_id: int
+    service_id: int
+
+    class Config:
+        from_attributes = True
+
+
+class UserServiceRoleCreate(BaseModel):
+    user_id: int
+    role_id: int
+    service_id: int
+
+
+class UserServiceRoleList(BaseModel):
+    user_service_roles: List[UserServiceRole]
+
+    class Config:
+        from_attributes = True
+
+
+class UserServiceRoleDelete(BaseModel):
+    user_id: Optional[int]
+    service_id: Optional[int]
+
+
+class UserServiceRoleResponse(BaseModel):
+    role_id: int
+    service_id: int
+
+
 class User(BaseModel):
     id: int
     username: str
@@ -11,7 +87,6 @@ class User(BaseModel):
     is_superuser: bool = False
 
     class Config:
-        orm_mode = True
         from_attributes = True
 
 
@@ -32,16 +107,17 @@ class UserResponse(BaseModel):
     username: str
     email: EmailStr
     is_superuser: bool
+    service_roles: List[UserServiceRoleResponse]
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class UserList(BaseModel):
     users: List[UserResponse]
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class UserCreateResponse(BaseModel):
@@ -50,71 +126,18 @@ class UserCreateResponse(BaseModel):
     email: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class UserUpdateResponse(BaseModel):
     user: UserUpdate
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class UserListResponse(BaseModel):
     users: List[User]
 
     class Config:
-        orm_mode = True
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class Service(BaseModel):
-    id: int
-    name: str
-
-    class Config:
-        orm_mode = True
-
-
-class ServiceCreate(BaseModel):
-    name: str
-
-
-class ServiceList(BaseModel):
-    services: List[Service]
-
-    class Config:
-        orm_mode = True
-
-
-class Role(BaseModel):
-    id: int
-    name: str
-
-    class Config:
-        orm_mode = True
-
-
-class RoleCreate(BaseModel):
-    name: str
-
-
-class RoleList(BaseModel):
-    roles: List[Role]
-
-    class Config:
-        orm_mode = True
-
-
-class UserRole(BaseModel):
-    id: int
-    user_id: int
-    role_id: int
-    service_id: int
-
-    class Config:
-        orm_mode = True
+        from_attributes = True
